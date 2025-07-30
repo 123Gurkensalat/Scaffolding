d1f72a4700b2e8ec881e3c44a34d9df006f733b1
Florian Zinck <florianzinck361@gmail.com>
2025-07-30 13:23:53 +0200

    Base template

--- CakeBuild/Program.cs
+++ CakeBuild/Program.cs
@@ -24,7 +24,7 @@ public static class Program
 
 public class BuildContext : FrostingContext
 {
-    public const string ProjectName = "SteamPower";
+    public const string ProjectName = "Scaffolding";
     public string BuildConfiguration { get; set; }
     public string Version { get; }
     public string Name { get; }
